0d2314424bb1c7113a34e9d7b5cec99eddcde7e9
malikhuzafah <huzafahrajput10@gmail.com>
2022-02-03 17:01:27 +0500

    c

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import './transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final Function deleteTransaction;
+  final List<Transaction> transactions;
+  TransactionList(this.transactions, this.deleteTransaction);
+  @override
+  Widget build(BuildContext context) {
+    print('build() TransactionList');
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No Transactions added yet!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView(
+            children:
+                // return Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin: EdgeInsets.symmetric(
+                //           vertical: 10,
+                //           horizontal: 15,
+                //         ),
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           '\$${transactions[index].amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         children: <Widget>[
+                //           Text(
+                //             transactions[index].title,
+                //             style: Theme.of(context).textTheme.headline6,
+                //           ),
+                //           Text(
+                //             DateFormat.yMMMd().format(transactions[index].date),
+                //             style: TextStyle(color: Colors.grey),
+                //           ),
+                //         ],
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //       ),
+                //     ],
+                //   ),
+                // );
+
+                transactions
+                    .map(
+                      (tx) => TransactionItem(
+                        key: ValueKey(tx.id),
+                        transaction: tx,
+                        deleteTransaction: deleteTransaction,
+                      ),
+                    )
+                    .toList(),
+          );
+  }
+}
